b83e65a0524a5f955b0626bd1679d130ea9d1032
hugofloter <hugo.paigneau@etu.utc.fr>
2019-09-20 14:40:03 +0200

    menu finished

--- src/components/contextmenu.jsx
+++ src/components/contextmenu.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 
 function changeMenu(showMenu, setShowMenu) {
 
@@ -8,9 +9,15 @@ function changeMenu(showMenu, setShowMenu) {
 
     useEffect(() => {
         if (showMenu === true) {
-            const menuComp = (<div className="menu-context" onClick={() => setShowMenu(!showMenu)}>
-                <span className="menu-icon expanded" />
+            const menuComp = (<div className="menu-context expanded">
+                <span className="menu-icon expanded" onClick={() => setShowMenu(!showMenu)}/>
                 <div className="menu-title"> MENU </div>
+                <div className="menu-options">
+                    <Link to="/" style={{ textDecoration: 'none', color: 'black' }}><div className="menu-row">Accueil</div></Link>
+                    <Link to="/voyage" style={{ textDecoration: 'none', color: 'black' }}><div className="menu-row">Le voyage</div></Link>
+                    <Link to="/station" style={{ textDecoration: 'none', color: 'black' }}><div className="menu-row">La station</div></Link>
+                    <Link to="/packs" style={{ textDecoration: 'none', color: 'black' }}><div className="menu-row">Les packs</div></Link>
+                </div>
             </div>);
 
             setMenuComponent(menuComp)
@@ -26,9 +33,10 @@ function changeMenu(showMenu, setShowMenu) {
     return MenuContext
 }
 
-export function ContextMenu(props) {
+export function ContextMenu() {
     const [showMenu, setShowMenu] = useState(false)
-
+    //@TODO : Check for current page and not display
+    //@TODO : Check if user in shotgun to add Mon Compte
     return <div className="menu" >
             {changeMenu(showMenu, setShowMenu)}
         </div>
--- src/components/station/station.jsx
+++ src/components/station/station.jsx
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from "react";
 import { connect } from "react-redux";
 import * as c from "../../skiutconstants";
-import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import { ContextMenu } from "../contextmenu"
+import stationTitle from "../../images/facts_titre.svg";
 
 function Station(props) {
 
@@ -12,8 +12,8 @@ function Station(props) {
         <div className="fullWidth montagnes">
             <div className="fade"/>
         </div>
-        <div>
-            STATION
+        <div className="station-title">
+            <object type="image/svg+xml" data={stationTitle}/>
         </div>
 
     </div>
