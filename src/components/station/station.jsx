0d1233ea607a406cf717f7741fe5d300d5b1377e
hugofloter <hugo.paigneau@etu.utc.fr>
2019-09-21 16:20:42 +0200

    adding svgs

--- src/components/station/station.jsx
+++ src/components/station/station.jsx
@@ -3,15 +3,20 @@ import { connect } from "react-redux";
 import * as c from "../../skiutconstants";
 import { withRouter } from "react-router-dom";
 import { ContextMenu } from "../contextmenu"
+import stationTitle from "../../images/facts_titre.svg";
 import stationFacts from "../../images/facts_station.svg";
-
+import valDallos from "../../images/val_dallos.svg";
 
 function Station(props) {
     return <div className="fullHeight fullWidth">
         <ContextMenu />
         <div className="station fullWidth">
-            <div className="station-title" />
-            <div className="station-cover" />
+            <div className="station-title" >
+                <object data={stationTitle} width="100%" />
+            </div>
+            <div className="station-cover" >
+                <object data={valDallos} width="100%" />
+            </div>
             <div className="station-info">
                 <object data={stationFacts} width="100%" />
             </div>
