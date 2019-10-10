75a5ec2c046c060b022030c229ee7034476272fc
quentin <quentin@quentin>
2019-10-10 20:54:10 +0200

    change voyage page

--- src/components/voyage/voyage.jsx
+++ src/components/voyage/voyage.jsx
@@ -1,30 +1,24 @@
 import React from "react";
 import { ContextMenu } from "../contextmenu"
-import voyageTitle from "../../images/titre_voyage.svg";
-import calendar from "../../images/calendar.svg";
-import textSensation from "../../images/text_sensation.svg";
-import textDefis from "../../images/text_defis.svg";
-import textSouvenirs from "../../images/text_souvenirs.svg";
-import textShotgun from "../../images/text_shotgun.svg";
 import PageTitle from "../common/pageTitle";
 
 export function VoyageComponent() {
     return <div className="fullHeight fullWidth">
         <ContextMenu />
         <div className="voyage fullWidth">
-            <PageTitle title={voyageTitle}/>
+            <PageTitle title="../images/titre_voyage.svg"/>
             <div className="voyage-content">
                 <div className="voyage-calendar">
-                    <object type="image/svg+xml" data={calendar} width="50%" />
+                    <img src="../images/calendar.svg" />
                 </div>
                 <div className="voyage-info">
-                    <div className="fullWidth info-dec"><object type="image/svg+xml" data={textSensation} width="50%" /></div>
-                    <div className="fullWidth info-dec2"><object type="image/svg+xml" data={textDefis} width="50%" /></div>
-                    <div className="fullWidth info-dec"><object type="image/svg+xml" data={textSouvenirs} width="50%" /></div>
+                    <div className="fullWidth info-dec"><img src="../images/text_sensation.svg"/></div>
+                    <div className="fullWidth info-dec2"><img src="../images/text_defis.svg"/></div>
+                    <div className="fullWidth info-dec"><img src="../images/text_souvenirs.svg"/></div>
                 </div>
             </div>
             <div className="fullWidth">
-                <object type="image/svg+xml" className="shotgun-info" data={textShotgun} width="50%" />
+                <img className="shotgun-info" src="../images/text_shotgun.svg"/>
             </div>
         </div>
     </div>
