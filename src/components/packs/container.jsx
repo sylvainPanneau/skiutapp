afc4478473d77e69b0c74e9725c8059b307cd3c8
quentin <quentin@quentin>
2019-10-10 20:26:53 +0200

    change pack page

--- src/components/packs/container.jsx
+++ src/components/packs/container.jsx
@@ -4,36 +4,28 @@ import InformationBlock from "./informationBlock";
 import PacksBlock from "./packsBlock";
 import "./css/container.scss";
 import PageTitle from "../common/pageTitle";
-import iconBase from "../../images/icone_base.svg";
-import iconPanier from "../../images/icone_nourriture_1.svg";
-import iconAssurance from "../../images/icone_assurance_1.svg";
-import tp from "../../images/titre_packs.svg";
-import packBronze from "../../images/pack_bronze.svg";
-import packArgent from "../../images/pack_argent.svg";
-import packOr from "../../images/pack_or.svg";
-import titreChoose from "../../images/titre_shoes.svg";
 
 const Pack = () => {
   return (<div className="fullHeight">
         <ContextMenu/>
         <div className="packs">
-          <PageTitle title={tp}/>
+          <PageTitle title="../images/titre_packs.svg"/>
           <div className="packs-content">
             <div className="packs-informations">
               <div className="row">
-                <InformationBlock icon={ iconBase } title="LE PACK DE BASE: 325€">
+                <InformationBlock icon="../images/icone_base.svg" title="LE PACK DE BASE: 325€">
                   Ce pack de base comporte tes trajets en navette aller retour, ta chambre ainsi que ton forfait de ski, les trois sont sympas pour monter en altitude.
                 <br/>
                   En plus tu as une assurance rapatriement comprise au cas où tu fais trop le fou, plutôt chouette non ?
                 </InformationBlock>
               </div>
               <div className="row">
-                <InformationBlock icon={ iconPanier } title="LE PACK DE NOURRITURE: 42€">
+                <InformationBlock icon="../images/icone_nourriture_1.svg" title="LE PACK DE NOURRITURE: 42€">
                   Avec ce pack tu auras toute la nourriture délicieuse que ton ventre demande tant.
                   <br/>
                   Eh oui, la glisse ça creuse!
                 </InformationBlock>
-                <InformationBlock icon={ iconAssurance } title="LES ASSURANCES">
+                <InformationBlock icon="../images/icone_assurance_1.svg" title="LES ASSURANCES">
                   "Parce qu'on sait jamais", c'est ce que tu peux te dire devant ton écran.
                   <br/>
                   Alors si tu es prévoyant et sécure, c'est simple:
@@ -44,10 +36,10 @@ const Pack = () => {
             </div>
             <div className="packs-shoes">
               <div className="packs-shoes-title">
-                <object type="image/svg+xml" data={ titreChoose }/>
+                <img src="../images/titre_shoes.svg"/>
               </div>
               <div className="packs-shoes-packs">
-                <PacksBlock name="PACK BRONZE" to="/packs/bronze" icon={ packBronze }>
+                <PacksBlock name="PACK BRONZE" to="/packs/bronze" icon="../images/pack_bronze.svg">
                   Pour découvrir la station en long et en large:
                   <br/>
                   <br/>
@@ -57,7 +49,7 @@ const Pack = () => {
                   <br/>
                   Chaussures seules: 35€
                 </PacksBlock>
-                <PacksBlock name="PACK ARGENT" to="/packs/argent" icon={ packArgent }>
+                <PacksBlock name="PACK ARGENT" to="/packs/argent" icon="../images/pack_argent.svg">
                   Envoie du lourd sur les pistes avec ce pack pour habitués:
                   <br/>
                   <br/>
@@ -67,7 +59,7 @@ const Pack = () => {
                   <br/>
                   Chaussures seules: 55€
                 </PacksBlock>
-                <PacksBlock name="PACK OR" to="/packs/or" icon={ packOr }>
+                <PacksBlock name="PACK OR" to="/packs/or" icon="../images/pack_or.svg">
                   Profite d'un max de sensations avec ce pack pour experts:
                   <br/>
                   <br/>
