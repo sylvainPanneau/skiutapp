889f257ddd147cf05ffde74a7d9f76900b26d9cc
Clement <clementlebre.cl@gmail.com>
2019-10-13 19:52:22 +0200

    Form V1

--- src/components/compte/compte.jsx
+++ src/components/compte/compte.jsx
@@ -12,7 +12,6 @@ import {changeInput} from "../login/utils/loginUtils";
 const Compte = ({userInfo, updateInfos}) => {
     const [formInfos, setFormInfos] = useState(userInfo)
 
-
     return <div className="fullWidth fullHeight">
         <ContextMenu />
         <div className="compte">
@@ -20,12 +19,14 @@ const Compte = ({userInfo, updateInfos}) => {
             <div className="recapTxt">
             <div> Bonjour { userInfo.login }, </div><br /><div>Tes options:</div><br />
             <ul>
-              <li> Email: {userInfo.email} </li>
-              <li> Tel: {userInfo.tel} </li>
-              <li> City: {userInfo.city} </li>
-              <li> Taille: <input type="number" value={formInfos.size || 0} onChange={(e) => changeInput(e, setFormInfos)}/> cm </li>
-              <li> Poids: {userInfo.weight} </li>
-              <li> Pointure: {userInfo.shoesize} </li>
+                <li> Email: <input name="email" type="text" value={formInfos.email || "email"} onChange={(e) => setFormInfos({ ...formInfos, email: e.target.value})}/> </li>
+                <li> Tel: <input name="tel" type="number" value={formInfos.tel || 0} onChange={(e) => setFormInfos({ ...formInfos, tel: e.target.value})}/> </li>
+                <li> City: <input name="city" type="text" value={formInfos.city || "ville"} onChange={(e) => setFormInfos({ ...formInfos, city: e.target.value})}/> 
+                    ZipCode: <input name="zipcode" type="number" value={formInfos.zipcode || 0} onChange={(e) => setFormInfos({ ...formInfos, zipcode: e.target.value})}/></li> 
+                <li> Adresse: <input name="address" type="text" value={formInfos.address || "adresse"} onChange={(e) => setFormInfos({ ...formInfos, address: e.target.value})}/> </li>
+                <li> Taille: <input name="size" type="number" value={formInfos.size || 0} onChange={(e) => setFormInfos({ ...formInfos, size: e.target.value})}/> cm </li>
+                <li> Poids: <input name="weight" type="number" value={formInfos.weight || 0} onChange={(e) => setFormInfos({ ...formInfos, weight: e.target.value})}/> kg </li>
+                <li> Pointure: <input name="shoesize" type="number" value={formInfos.shoesize || 0} onChange={(e) => setFormInfos({ ...formInfos, shoesize: e.target.value})}/> </li>
             </ul>
             </div>
           </div>
